009852263282b01c40fd9fa9b08b85af4c05f296
p1-BCMC <68663569+p1-BCMC@users.noreply.github.com>
2020-07-22 21:43:39 +0200

    Create PerspectiveAdjustment.js

---
+++ PerspectiveAdjustment.js
@@ -0,0 +1,109 @@
+// ==UserScript==
+// @name         Perspective Adjustment
+// @namespace    p1
+// @run-at       document-start
+// @version      0.1
+// @description  Adjusts the size of the player depending on how close/far they are in a room.
+// @author       p1
+// @match        https://boxcritters.com/play/
+// @match        https://boxcritters.com/play/?*
+// @match        https://boxcritters.com/play/#*
+// @match        https://boxcritters.com/play/index.html
+// @match        https://boxcritters.com/play/index.html?*
+// @match        https://boxcritters.com/play/index.html#*
+// @grant        none
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+
+    let timer = setInterval(function() {
+		if (typeof world !== "undefined" && typeof world.stage !== "undefined" && typeof world.stage.room !== "undefined") {
+			clearInterval(timer);
+			onWorldLoaded();
+		}
+	}, 1000/60);
+
+	function onWorldLoaded() {
+		var scaleNear = 1.225;
+		var scaleFar = 0.775;
+		var movementDistanceForScaleChange = 3; /* in ingame units */
+		var updateInterval = movementDistanceForScaleChange * 5; /* critters move 200 units/s ; and in ms*/
+		var currentlyMovingPlayerIds = [];
+
+		function updateScale(playerId) {
+			let player = world.stage.room.getPlayer(playerId);
+			player.scale = scaleFar + (scaleNear - scaleFar) * player.y / world.stage.room.height;
+		};
+
+		function checkPlayer(playerId) {
+			let player = world.stage.room.getPlayer(playerId);
+			if (player.x != player.targetX || player.y != player.targetY) {
+				currentlyMovingPlayerIds.push(playerId);
+			};
+		};
+
+		function checkAllPlayers() {
+			if (world.stage.room.getPlayer(world.player.playerId) == undefined) {
+				setTimeout(function() {checkAllPlayers()}, 1000/60);
+			} else {
+				currentlyMovingPlayerIds = [];
+				world.room.playerCrumbs.forEach(playerCrumb => {checkPlayer(playerCrumb.i); updateScale(playerCrumb.i);});
+				if (currentlyMovingPlayerIds.length > 0) {
+					setTimeout(function() {updateScalesScheduler();}, updateInterval);
+				};
+			};
+		};
+
+		function updatePlayer(playerId) {
+			let player = world.stage.room.getPlayer(playerId);
+			player.scale = scaleFar + (scaleNear - scaleFar) * player.y / world.stage.room.height;
+			if (player.x == player.targetX && player.y == player.targetY) {
+				currentlyMovingPlayerIds.splice(currentlyMovingPlayerIds.indexOf(playerId), 1);
+			};
+		};
+
+		function updateScalesScheduler() {
+			currentlyMovingPlayerIds.forEach(playerId => {updatePlayer(playerId)});
+			if (currentlyMovingPlayerIds.length > 0) {
+				setTimeout(function() {updateScalesScheduler();}, updateInterval);
+			};
+		};
+
+		world.on("joinRoom", function () {
+			currentlyMovingPlayerIds = [];
+			checkAllPlayers();
+		});
+
+		world.on("login", function () {
+			checkAllPlayers();
+		});
+
+		world.on("A", function (playerJoinEvent) {
+			let player = world.stage.room.getPlayer(playerJoinEvent.i);
+			player.scale = scaleFar + (scaleNear - scaleFar) * player.y / world.stage.room.height;
+		});
+
+		world.on("X", function (playerMoveEvent) {
+			let wasAlreadyMoving = false; /* necessary if player clicks again before reaching target */
+			if (currentlyMovingPlayerIds.indexOf(playerMoveEvent.i) == -1) {
+				currentlyMovingPlayerIds.push(playerMoveEvent.i);
+			} else {
+				wasAlreadyMoving = true;
+			};
+			if (currentlyMovingPlayerIds.length == 1 && wasAlreadyMoving == false) {
+				setTimeout(function() {updateScalesScheduler();}, updateInterval);
+			}; /* otherwise, we are already going to update! */
+		});
+
+		world.on("R", function (playerRemoveEvent) {
+			let movingPlayerIndex = currentlyMovingPlayerIds.indexOf(playerRemoveEvent.i);
+			if (movingPlayerIndex != -1) {
+				currentlyMovingPlayerIds.splice(movingPlayerIndex, 1);
+			};
+		});
+
+		checkAllPlayers();
+	};
+})();
